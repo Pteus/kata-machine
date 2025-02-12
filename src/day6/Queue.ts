c5bbcfd4cdde9db9a49ec1ab056810359df979fd
Pedro Mateus <pedropmateus91@gmail.com>
2025-02-12 22:36:36 +0000

    queue

--- src/day6/Queue.ts
+++ src/day6/Queue.ts
@@ -1,18 +1,42 @@
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head?: ListNode<T>;
+  private tail?: ListNode<T>;
 
-    
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    const newNode: ListNode<T> = { value: item };
+
+    this.length++;
+
+    if (!this.tail) {
+      this.head = this.tail = newNode;
+      return;
     }
 
-    enqueue(item: T): void {
+    this.tail.next = newNode;
+    this.tail = newNode;
+  }
+  deque(): T | undefined {
+    if (!this.head) {
+      return undefined;
+    }
 
-}
-    deque(): T | undefined {
+    this.length--;
 
-}
-    peek(): T | undefined {
+    const head = this.head;
+    this.head = this.head.next;
 
-}
+    if (this.length === 0) {
+      this.tail = undefined;
+    }
+    return head.value;
+  }
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
