d073a3d0d65ccaff431afd8f559e93c9795c3a76
Pedro Mateus <pedropmateus91@gmail.com>
2025-02-13 20:16:46 +0000

    graphs

--- src/day6/DFSGraphList.ts
+++ src/day6/DFSGraphList.ts
@@ -1,3 +1,46 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+export default function dfs(
+  graph: WeightedAdjacencyList,
+  source: number,
+  needle: number,
+): number[] | null {
+  let path: number[] = [];
+  let seen: boolean[] = new Array(graph.length).fill(false);
 
+  search(graph, source, needle, seen, path);
+
+  if (path.length === 0) {
+    return null;
+  }
+
+  return path;
 }
+
+function search(
+  graph: WeightedAdjacencyList,
+  curr: number,
+  needle: number,
+  seen: boolean[],
+  path: number[],
+) {
+  if (seen[curr]) {
+    return false;
+  }
+
+  seen[curr] = true;
+  path.push(curr);
+
+  if (curr === needle) {
+    return true;
+  }
+
+  const list = graph[curr];
+  for (let i = 0; i < list.length; i++) {
+    if (search(graph, list[i].to, needle, seen, path)) {
+      return true;
+    }
+  }
+
+  path.pop();
+  return false;
+}
+
